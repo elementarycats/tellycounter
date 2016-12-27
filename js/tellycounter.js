17ac99209fcfa0ffb51f2d7e4518690562f44e93
ElGatoSaez <geniointernet@gmail.com>
2016-12-27 03:14:24 -0300

    Creating p create function
    strangers in the night by frank sinatra. This is also my first time
    using Atom

--- js/tellycounter.js
+++ js/tellycounter.js
@@ -8,3 +8,12 @@ function difference() {
     counter--;
     document.getElementById('counter-html').innerHTML = counter;
 }
+function replaceP() {
+  var titleName = document.getElementById('title').value;
+  var pUsed = document.createElement('p');
+  var nodeforP = document.createTextNode(titleName);
+  pUsed.appendChild(nodeforP);
+  var parent = document.getElementsByTagName('title-div'); // testing this stuff
+  var child = document.getElementById('title');
+  parent.replaceChild(pUsed, child)
+}
