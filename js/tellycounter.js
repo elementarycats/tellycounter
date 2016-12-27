fa8f21a75c31703871b8c84738dfa2a6d56f3a02
ElGatoSaez <geniointernet@gmail.com>
2016-12-27 02:00:54 -0300

    Revert "This commit finally made the core counter working"
    This reverts commit 960baf917360c38612adc65e895f40ce7c767562.

--- js/tellycounter.js
+++ js/tellycounter.js
@@ -1,10 +1,11 @@
 var counter = 000;
-document.getElementById('counter-html').innerHTML = counter;
+
 function sum() {
     counter++;
-    document.getElementById('counter-html').innerHTML = counter;
 }
+
 function difference() {
     counter--;
-    document.getElementById('counter-html').innerHTML = counter;
 }
+
+document.getElementById('counter-html').innerHTML = counter;
