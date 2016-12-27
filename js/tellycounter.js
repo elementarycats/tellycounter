960baf917360c38612adc65e895f40ce7c767562
ElGatoSaez <geniointernet@gmail.com>
2016-12-27 01:58:45 -0300

    This commit finally made the core counter working
    fa fa fa by datarock

--- js/tellycounter.js
+++ js/tellycounter.js
@@ -1,11 +1,10 @@
 var counter = 000;
-
+document.getElementById('counter-html').innerHTML = counter;
 function sum() {
     counter++;
+    document.getElementById('counter-html').innerHTML = counter;
 }
-
 function difference() {
     counter--;
+    document.getElementById('counter-html').innerHTML = counter;
 }
-
-document.getElementById('counter-html').innerHTML = counter;
